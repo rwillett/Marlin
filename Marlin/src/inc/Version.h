e06467850485b7b11f33e4665af424a698fcbcbb
allted <allted@gmail.com>
2020-04-04 08:01:06 -0700

    no message

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "421 bugfix-2.0.x"
+  #define SHORT_BUILD_VERSION "425 bugfix-2.0.x"
 #endif
 
 /**
